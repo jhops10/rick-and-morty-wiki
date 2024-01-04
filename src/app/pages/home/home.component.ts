b44da6eebc704ec1be4b77e12a2fe3e2c3bbf8d0
João Paulo <joaowais@gmail.com>
2024-01-04 14:58:18 -0300

    adicionado funcionalidade ao input search

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Character } from 'src/app/models/character.model';
 import { CharacterService } from 'src/app/services/character.service';
 
 @Component({
@@ -8,31 +7,34 @@ import { CharacterService } from 'src/app/services/character.service';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  data: any;
-  characters: Character[] = [];
-
+  data: any[] = [];
+  searchData: any[] = [];
   searchTerm: string = '';
 
   constructor(private characterService: CharacterService) {}
 
   ngOnInit(): void {
     this.characterService.getCharacters().subscribe((res) => {
-      this.data = res.results;
-      this.characters = this.characters.concat(this.data);
-      console.log(this.characters);
+      this.data = this.data.concat(res.results);
     });
   }
 
   loadMore() {
     this.characterService.loadMore().subscribe((res) => {
       this.data = this.data.concat(res.results);
-      this.characters = this.characters.concat(this.data);
     });
   }
 
-  search() {
-    this.characterService.searchByName(this.searchTerm).subscribe((res) => {
-      this.data = res.results;
+  searchByName() {
+    this.searchData = this.data.filter((character) => {
+      return character.name
+        .toLowerCase()
+        .includes(this.searchTerm.toLowerCase());
     });
+  }
+
+  resetSearch() {
+    this.searchTerm = '';
+    this.searchData = [];
   }
 }
